9e2b6ac87e3bdc84aac9c3360d71bbe41b8cca84
Abhinandan Raj <abhinandanraj199@gmail.com>
2021-01-27 01:32:31 +0530

    Create Swap Two Numbers Without Using Third Variable.py

---
+++ Self Practice Problems/Swap Two Numbers Without Using Third Variable.py
@@ -0,0 +1,12 @@
+# Write a program to Swap Two Numbers Without Using Third Variable.
+
+n1=int(input())
+n2=int(input())
+
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+
+print("After Swapping")
+print(n1)
+print(n2)
