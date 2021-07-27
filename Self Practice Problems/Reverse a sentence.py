ef6a4705c35b470e5cdfb1391f2a67581cc03a3e
Abhinandan Raj <abhinandanraj199@gmail.com>
2021-07-28 02:24:51 +0530

    Create Reverse a sentence.py

---
+++ Self Practice Problems/Reverse a sentence.py
@@ -0,0 +1,17 @@
+# Reverse the sentence
+# ----------Method 1--------------------
+
+def sentence_reverse(text):
+  wordlist = text.split()
+  reversed_list = wordlist[:: -1]
+  reversed_sentence = " ".join(reversed_list)
+  return reversed_sentence  
+
+if __name__ == "__main__":
+  sentence = input()
+  print(sentence_reverse(sentence))
+
+#-----------Method 2------------------
+  
+sentence = input("Enter a sentence :")
+print(" ".join(reversed(sentence.split())))
